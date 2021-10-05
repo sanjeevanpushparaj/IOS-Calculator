122445ed2aef31a80f7fa3d04187b6fb630b4777
Sanjeevan Pushparaj <sanjeevaninoj@gmail.com>
2021-10-04 23:33:02 -0400

    creating the logic for the design

--- sample/ViewController.swift
+++ sample/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Creating Outlets
     @IBOutlet weak var btnDot: UIButton!
     @IBOutlet weak var btn0: UIButton!
     @IBOutlet weak var btn9: UIButton!
@@ -23,10 +24,119 @@ class ViewController: UIViewController {
     @IBOutlet weak var btn2: UIButton!
     @IBOutlet weak var btn1: UIButton!
     @IBOutlet weak var textView: UILabel!
+    
+    var display:Double=0
+    var exDisplay:Double=0
+    var result=false
+    var calculation:Int=0
+    var check:Double=0
+    var temp:String=""
+    
+    
+    @IBAction func numbers(_ sender: UIButton) {
+        
+        if result==true{
+            
+            textView.text=String(sender.tag-1)
+            display=Double(textView.text!)!
+            result=false
+        }
+        else if sender.tag==20{
+            
+            textView.text = textView.text!+"."
+            display=Double(textView.text!)!
+        }
+        else{
+            textView.text = textView.text! + String(sender.tag-1)
+            display=Double(textView.text!)!
+        }        
+       
+    }
+    
+    
+    @IBAction func operation(_ sender: UIButton) {
+        
+        if textView.text != "" && ((sender.tag>10 && sender.tag<15)||(sender.tag==17)){
+            
+            exDisplay=Double(textView.text!)!
+            if sender.tag==11{
+                textView.text="+"
+            }
+            if sender.tag==12{
+                textView.text="-"
+            }
+            if sender.tag==13{
+                textView.text="*"
+            }
+            if sender.tag==14{
+                textView.text="÷"
+            }
+            if sender.tag==17{
+                textView.text="%"
+            }
+            calculation=sender.tag
+            result=true
+        }
+        else if sender.tag==15{
+            
+            if calculation==11{
+                check=Double(exDisplay+display)
+                textView.text=formatResult(result:check)
+            }
+            if calculation==12{
+                check=Double(exDisplay-display)
+                textView.text=formatResult(result:check)
+
+            }
+            if calculation==13{
+                check=Double(exDisplay*display)
+                textView.text=formatResult(result:check)
+            }
+            if calculation==14{
+                check=Double(exDisplay/display)
+                textView.text=formatResult(result:check)
+            }
+            if calculation==17{
+                check=Double((exDisplay/100)*display)
+                textView.text=formatResult(result:check)
+            }
+            
+        }
+        else if sender.tag==16{
+            textView.text=""
+            exDisplay=0
+            display=0
+            calculation=0
+        }
+        else if sender.tag==18{
+            display=display*(-1)
+            textView.text=String(display)
+        }
+        else if sender.tag==19{
+            temp=textView.text!
+            temp.removeLast()
+            textView.text=temp
+            
+        }
+        
+    }
+    
+    func formatResult(result: Double) -> String
+    {
+        if(result.truncatingRemainder(dividingBy: 1) == 0)
+        {
+            return String(format: "%.0f", result)
+        }
+        else
+        {
+            return String(format: "%.8f", result)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
-        
         textView.layer.cornerRadius = 10
         textView.layer.masksToBounds = true
         
@@ -64,7 +174,5 @@ class ViewController: UIViewController {
         btn0.layer.masksToBounds = true
     }
     
-
-
 }
 
